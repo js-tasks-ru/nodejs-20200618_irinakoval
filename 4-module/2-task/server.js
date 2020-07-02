adf3b61b4351dba99e88f38848ef0d2b9307881b
IrinaKoval <irina.koval@oliverwyman.com>
2020-07-02 14:21:54 +0300

    4-module 2-task: added fix for tests

--- 4-module/2-task/server.js
+++ 4-module/2-task/server.js
@@ -8,10 +8,45 @@ const LimitExceededError = require('./LimitExceededError');
 const server = new http.Server();
 
 server.on('request', (req, res) => {
+  const pathname = url.parse(req.url).pathname.slice(1);
+  const filepath = path.join(__dirname, 'files', pathname);
+  let writeStream;
+
   try {
-    const pathname = url.parse(req.url).pathname.slice(1);
-    const filepath = path.join(__dirname, 'files', pathname);
+    req.on('aborted', (error) => {
+      writeStream.destroy();
+      fs.unlink(filepath, (err) => {
+        if (err) {
+          res.statusCode = 500;
+          res.end(err.message);
+        }
+      });
+
+      res.statusCode = 500;
+      res.end('aborted');
+    });
+
     const limitSizeStream = new LimitSizeStream({limit: 1000000});
+    limitSizeStream.on('error', (err) => {
+      if (err instanceof LimitExceededError) {
+        res.statusCode = 413;
+      } else {
+        res.statusCode = 500;
+      }
+      if (writeStream) writeStream.destroy();
+      fs.unlink(filepath, (err) => {
+        if (err) {
+          res.statusCode = 500;
+          res.end(err.message);
+        }
+        // res.end();
+      });
+      // fix for tests
+      res.write('');
+
+      // fix for tests
+      setTimeout(() => res.end(err.message), 1000);
+    });
 
     switch (req.method) {
       case 'POST':
@@ -21,60 +56,20 @@ server.on('request', (req, res) => {
           return;
         }
 
-        const writeStream = fs.createWriteStream(filepath, {flags: 'wx'});
-        limitSizeStream.pipe(writeStream);
-
-        limitSizeStream.on('error', (err) => {
-          if (err instanceof LimitExceededError) {
-            res.statusCode = 413;
-          } else {
-            res.statusCode = 500;
-          }
-          limitSizeStream.resume();
-          writeStream.end();
-          fs.unlink(filepath, (err) => {
-            if (err) {
-              res.statusCode = 500;
-              res.end(err.message);
-            }
-          });
-          res.end(err.message);
-        });
+        writeStream = fs.createWriteStream(filepath, {flags: 'wx'});
 
         writeStream.on('error', (err) => {
-          if ( err.code === 'EEXIST') {
+          if (err.code === 'EEXIST') {
+            if (writeStream) writeStream.destroy();
             res.statusCode = 409;
+            res.end(err.message);
           }
-          res.end(err.message);
         }).on('finish', () => {
           res.statusCode = 201;
           res.end('done');
         });
 
-        req.on('data', (chunk) => {
-          limitSizeStream.write(chunk);
-        }).on('end', () => {
-          limitSizeStream.resume();
-          writeStream.end();
-        }).on('error', (error) => {
-          writeStream.end();
-          fs.unlink(filepath, (err) => {
-            res.statusCode = 500;
-            res.end(error.message);
-          });
-          res.statusCode = 500;
-          res.end(error.message);
-        }).on('aborted', (error) => {
-          writeStream.end();
-          fs.unlink(filepath, (err) => {
-            if (err) {
-              res.statusCode = 500;
-              res.end(err.message);
-            }
-          });
-          res.statusCode = 500;
-          res.end('aborted');
-        });
+        req.pipe(limitSizeStream).pipe(writeStream);
         break;
 
       default:
@@ -88,3 +83,5 @@ server.on('request', (req, res) => {
 });
 
 module.exports = server;
+
+
