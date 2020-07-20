bef6a07f12d0a2d91f05d1d4b1b36c5080c2988c
IrinaKoval <irina.koval@oliverwyman.com>
2020-07-20 10:13:47 +0300

    9-module 1-task: done

--- 9-module/1-task/socket.js
+++ 9-module/1-task/socket.js
@@ -7,11 +7,31 @@ function socket(server) {
   const io = socketIO(server);
 
   io.use(async function(socket, next) {
+    if (!socket.user) {
+      const {token} = socket.handshake.query;
+      if (!token) {
+        return next(new Error('anonymous sessions are not allowed'));
+      }
+      const session = await Session.findOne({token}).populate('user');
+      if (!session) {
+        return next(new Error('wrong or expired session token'));
+      }
+      socket.user = session.user;
+    }
     next();
   });
 
   io.on('connection', function(socket) {
-    socket.on('message', async (msg) => {});
+    socket.on('message', async (msg) => {
+      const message = new Message({
+        date: new Date(),
+        text: msg,
+        chat: socket.user.id,
+        user: socket.user.displayName,
+      });
+      await message.save();
+      socket.emit('message', msg);
+    });
   });
 
   return io;
